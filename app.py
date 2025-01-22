0e0234133e87ee6458887749f6bb8f58d62219d8
Miguel Breceda de Lara <100646882+mbreceda@users.noreply.github.com>
2025-01-22 12:16:36 -0600

    Add Flask smorest and some other validations

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import items, stores
 
 app = Flask(__name__)
@@ -9,31 +10,54 @@ app = Flask(__name__)
 def get_store():
     return { "stores": list(stores.values()) }
 
+
 @app.get("/store/<string:store_id>")
 def get_store_by_id(store_id):
-        try: 
-            return stores[store_id]
-        except KeyError: 
-            return { "message": "Store not found" }, 404
+    try: 
+        return stores[int(store_id)]
+    except KeyError: 
+        abort(404, message="Store not found")
+
 
 @app.post('/store')
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="Missing required fields")
+
+    for store in store.values():
+        if store["name"] == store_data["name"]:
+            abort(400, message="Store already exists")
+
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id }
     stores[store_id] = store
     return store, 201
 
+# ITEMS
+
 @app.post('/item')
 def create_item():
     item_data = request.get_json()
+    if (
+        "price" not in item_data 
+        or "name" not in item_data
+        or "store_id" not in item_data
+    ): 
+        abort(400, message="Missing required fields")
+
+    for item in item.values():
+        if item["name"] == item_data["name"]:
+            abort(400, message="Item already exists")
+
     if item_data["store_id"] not in stores:
-        return { "message": "Store not found" }, 404
+        abort(404, message="Store not found")
     
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id }
     items[item_id] = item
     return item, 201
+
 
 @app.get('/item')
 def get_all_items():
@@ -43,6 +67,6 @@ def get_all_items():
 @app.get('/item/<string:item_id>')
 def get_item_by_id(item_id):
     try:
-        return items[item_id]
+        return items[int(item_id)]
     except KeyError:
         return { "message": "Item not found" }, 404
--- db.py
+++ db.py
@@ -1,4 +1,7 @@
-stores = {}
+stores = {
+    1: { "name": "store1", "items": [1] },
+    2: { "name": "store2", "items": [2] },
+}
 items = {
     1: { "name": "item1", "price": 10.99 },
     2: { "name": "item2", "price": 20.99 },
